0a75705147c63e79da1d39e88a5ff10848fb31fe
Diego Nova Rosas <diegonova368@gmail.com>
2026-04-12 20:48:00 -0500

    borrando comentarios

--- Codigo/main.py
+++ Codigo/main.py
@@ -19,24 +19,6 @@ def cargar_datos(ruta_ratings, ruta_movies):
 
     return usuarios_dict, mapeo_nombres
 
-#==CARGA ANTERIOR CON EXCEL B[ASICO]===#
-# # Intentamos leer detectando el separador automáticamente
-#     # engine='python' permite que sep=None funcione mejor
-#     df = pd.read_csv(ruta, sep=None, engine='python', index_col=0)
-    
-#     # Limpiamos posibles espacios en blanco en los nombres de columnas y filas
-#     df.index = df.index.str.strip()
-#     df.columns = df.columns.str.strip()
-
-#     # Transponemos: Ahora las columnas (Angelica, Bill...) son las filas (Índice)
-#     df_transpuesto = df.transpose()
-    
-#     # Convertimos a diccionario
-#     diccionario = df_transpuesto.to_dict(orient='index')
-    
-#     return diccionario
-
-
 
 
 def main():
@@ -63,28 +45,6 @@ def main():
     for movie_id, prediccion in propuestas[:10]: # Top 10
         titulo = mapeo_nombres.get(movie_id, f"Desconocida (ID:{movie_id})")
         print(f"- {titulo:50} | Predicción: {prediccion:.2f} estrellas")
-
-
-    # usuarios_dict = cargar_datos("DatasetEjemplo.csv")
-    # print("Usuarios detectados:", list(usuarios_dict.keys()))
-    # usuario_objetivo = "Angelica"
-    # k = 5
-    # print(f"Calculando vecinos para {usuario_objetivo}...")
-
-    # vecinos = knn(usuario_objetivo, usuarios_dict, k)
-
-    # print(f"\nLos {k} usuarios más parecidos a {usuario_objetivo} son:")
-    # for nombre, score in vecinos:
-    #     print(f"- {nombre}: {score:.4f}")
-    # #===========recomendacion=========#
-
-    # recomendaciones = recomendacion(usuario_objetivo, usuarios_dict, vecinos)
-    
-    # print(f"\n las recomendaciones son:")
-    # for artista, prediccion in recomendaciones:
-    #     print(f"- {artista}: {prediccion:.4f}")
-
-
 
 if __name__ == "__main__":
     main()
